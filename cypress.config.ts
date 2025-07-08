8a34c238bf524d7d0cd22f26857f2d9cc3528fcb
muhdmokhtar11git config --global user.name muhdmokhtar11 <muhammadmokhtar06@gmail.com>
2025-07-08 19:43:30 +0800

    Update Cypress configuration for coverage URL, modify package.json scripts for concurrent execution, and adjust Webpack devtool settings for improved source mapping.

--- cypress.config.ts
+++ cypress.config.ts
@@ -16,8 +16,7 @@ export default defineConfig({
     jwtStorageName: 'jhi-authenticationToken',
     coverage: true,
     codeCoverage: {
-      url: '/api/__coverage__',
-      exclude: ['src/test/**/*', 'src/main/webapp/app/**/*.spec.ts', 'src/main/webapp/app/**/*.test.ts'],
+      url: 'http://localhost:8080/__coverage__',
     },
   },
   e2e: {
@@ -25,10 +24,11 @@ export default defineConfig({
     // You may want to clean this up later by importing these.
     async setupNodeEvents(on, config) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return require('./src/test/javascript/cypress/plugins/index.ts').default(on, config);
+      return (await import('./src/test/javascript/cypress/plugins/index')).default(on, config);
     },
     baseUrl: 'http://localhost:8080/',
     specPattern: 'src/test/javascript/cypress/e2e/**/*.cy.ts',
     supportFile: 'src/test/javascript/cypress/support/index.ts',
+    experimentalRunAllSpecs: true,
   },
 });
